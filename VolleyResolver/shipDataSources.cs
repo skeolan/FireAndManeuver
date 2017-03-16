f7b38ae67754ded55bde6c569b8418da9580ba19
Patrick Maughan <pmaughan@microsoft.com>
2017-03-16 16:24:43 -0700

    variable ship-data-source configs

---
+++ VolleyResolver/shipDataSources.cs
@@ -0,0 +1,12 @@
+namespace VolleyResolver
+{
+    public class shipDataSources
+    {
+        public shipDataSources()
+        {
+            //No data sources!
+        }
+        public string[] srcDirectories { get; set; }
+        public string[] srcFiles { get; set; }
+    }
+}
