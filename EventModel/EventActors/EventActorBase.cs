fb6c6de860ce60b6b379ca1bfb7c152bfdb18349
Patrick Maughan <pmaughan@microsoft.com>
2018-03-02 16:44:27 -0800

    abstract base class to handle event-type routing to event-type-specific virtual methods. Ooooh, aren't we fancy.

---
+++ EventModel/EventActors/EventActorBase.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="EventActorBase.cs" company="Patrick Maughan">
+// Copyright (c) Patrick Maughan. All rights reserved.
+// </copyright>
+
+namespace FireAndManeuver.EventModel.EventActors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public abstract class EventActorBase : IEventActor
+    {
+        public int EventReceivedCount { get; protected set; } = 0;
+
+        protected List<GameEvent> Result { get; set; } = new List<GameEvent>();
+
+        public virtual List<GameEvent> ProcessEvent(GameEvent evt)
+        {
+            this.EventReceivedCount++;
+
+            if (evt is GamePhaseEvent)
+            {
+                this.ProcessGamePhaseEvent(evt as GamePhaseEvent);
+            }
+
+            if (evt is WeaponAttackEvent)
+            {
+                this.ProcessWeaponAttackEvent(evt as WeaponAttackEvent);
+            }
+
+            return this.Result;
+        }
+
+        protected virtual void ProcessWeaponAttackEvent(WeaponAttackEvent weaponAttackEvent)
+        {
+            // no-op
+        }
+
+        protected virtual void ProcessGamePhaseEvent(GamePhaseEvent evt)
+        {
+            // no-op
+        }
+    }
+}
